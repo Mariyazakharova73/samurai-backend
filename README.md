16137eb9e2873ef3a7f07d30257325aa441bfca1
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-09-02 22:15:34 +0400

    added request delete, put

---
+++ README.md
@@ -0,0 +1,33 @@
+### samurai-backend
+
+#### О проекте
+
+test express
+
+#### Технологии
+
+<div>
+  <img height='25px' src="https://img.shields.io/badge/Express-20232A??style=plastic&logo=Express&logoColor=fff" alt="Express">
+  <img height='25px' src="https://img.shields.io/badge/TypeScript-20232A??style=plastic&logo=typescript&logoColor=3178C6" alt="TypeScript.">
+  <img height='25px' src="https://img.shields.io/badge/MongoDB-20232A??style=plastic&logo=MongoDB&logoColor=47A248" alt="MongoDB">
+ 
+</div>
+
+#### Установка и запуск приложения
+
+<details><summary><b>Развернуть</b></summary>
+
+Клонировать репозиторий:
+
+    git clone https://github.com/Mariyazakharova73/samurai-backend.git
+
+Установить зависимости:
+
+    yarn add
+
+Запустить проект:
+
+    yarn dev
+    yarn watch
+
+</details>
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import express from "express";
+const app = express();
+const port = 3001;
+
+const HTTP_STATUSES = {
+  OK_200: 200,
+  CREATED_201: 201,
+  NO_CONTENT_204: 204,
+
+  BAD_REQUEST_400: 400,
+  NOT_FOUND_404: 404,
+};
+
+const jsonBodyMiddleware = express.json();
+app.use(jsonBodyMiddleware);
+
+let db = {
+  courses: [
+    { id: 0, title: "front" },
+    { id: 1, title: "back" },
+    { id: 3, title: "testback" },
+    { id: 4, title: "testfront" },
+  ],
+};
+
+app.get("/courses", (req, res) => {
+  let foundCourses = db.courses;
+  if (req.query.title) {
+    foundCourses = foundCourses.filter((c) => c.title.indexOf(req.query.title as string) > -1);
+  }
+
+  res.json(foundCourses);
+});
+
+app.get("/courses/:id", (req, res) => {
+  const course = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!course) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  res.json(course);
+});
+
+app.post("/courses", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+    return;
+  }
+
+  const createdCourse = { id: +new Date(), title: req.body.title };
+  db.courses.push(createdCourse);
+  res.status(HTTP_STATUSES.CREATED_201).json(createdCourse);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  db.courses = db.courses.filter((c) => c.id !== +req.params.id);
+  res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
+});
+
+app.put("/courses/:id", (req, res) => {
+  if (!req.body.title) {
+    res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
+    return;
+  }
+
+  const course = db.courses.find((c) => c.id === +req.params.id);
+
+  if (!course) {
+    res.sendStatus(HTTP_STATUSES.NOT_FOUND_404);
+    return;
+  }
+
+  course.title = req.body.title;
+
+  res.status(HTTP_STATUSES.NO_CONTENT_204).json(course);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
